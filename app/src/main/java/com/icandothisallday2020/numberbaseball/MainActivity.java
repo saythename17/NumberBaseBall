c3546fe95e3aa639aa3f45dfb4d59e2facc08e64
FANXY_ON <zion.h719@gmail.com>
2020-07-03 14:52:11 +0900

    message

--- app/src/main/java/com/icandothisallday2020/numberbaseball/MainActivity.java
+++ app/src/main/java/com/icandothisallday2020/numberbaseball/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         int a=10;
-        double b=3.14;
+        double b=1.5;
         char c='a';
         //Random rnd=new Random();
 //        int h;
